1d4e387a01ceb70147ba599a689f5518c7282557
WiccaCrow <Selletrer@mail.ru>
2022-12-01 16:40:44 +0300

    added information to the info_vagrantfile.txt

--- p1/scripts/runS.sh
+++ p1/scripts/runS.sh
@@ -6,4 +6,7 @@ sudo sed -i -e "s|mirrorlist=|#mirrorlist=|g" /etc/yum.repos.d/CentOS-*
 sudo sed -i -e "s|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g" /etc/yum.repos.d/CentOS-*
 
 # install k3s
-curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server" K3S_KUBECONFIG_MODE="644" sh -
+# https://docs.k3s.io/advanced#additional-preparation-for-red-hatcentos-enterprise-linux
+yum install -y container-selinux selinux-policy-base
+yum install -y https://rpm.rancher.io/k3s/latest/common/centos/8/noarch/k3s-selinux-0.2-1.el7_8.noarch.rpm
+curl -sfL https://get.k3s.io | INSTALL_K3S_SELINUX_WARN=true INSTALL_K3S_EXEC="server --selinux" sh -
---
+++ p1/scripts/runSW.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# for yum in CentOS-8
+# https://forketyfork.medium.com/centos-8-no-urls-in-mirrorlist-error-3f87c3466faa
+sudo sed -i -e "s|mirrorlist=|#mirrorlist=|g" /etc/yum.repos.d/CentOS-*
+sudo sed -i -e "s|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g" /etc/yum.repos.d/CentOS-*
+
+# install k3s
+# curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="agent" K3S_KUBECONFIG_MODE="644" sh -
