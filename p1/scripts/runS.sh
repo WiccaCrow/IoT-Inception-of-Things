ca47ca2e95e92490e860b4a392749ecaf96b8517
WiccaCrow <selletrer@mail.ru>
2022-12-06 17:01:39 +0300

    p1: fix all
    Co-authored-by: mhufflep <mhufflep@student.21-school.ru>

--- p1/scripts/runS.sh
+++ p1/scripts/runS.sh
@@ -1,18 +1,21 @@
 #!/bin/sh
 
-# for yum in CentOS-8
-# https://forketyfork.medium.com/centos-8-no-urls-in-mirrorlist-error-3f87c3466faa
-sudo sed -i -e "s|mirrorlist=|#mirrorlist=|g" /etc/yum.repos.d/CentOS-*
-sudo sed -i -e "s|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g" /etc/yum.repos.d/CentOS-*
-
 # install k3s
-curl -sfL https://get.k3s.io |              \
-    INSTALL_K3S_SELINUX_WARN=true           \
-    K3S_KUBECONFIG_MODE="644"               \
-    INSTALL_K3S_EXEC="server                \
-        --selinux                           \
-        --cluster-init                      \
-        --bind-address=192.168.56.111       \
-        --advertise-address=192.168.56.111"  sh -
-    #     --node-id=192.168.56.111"           \
-    # sh -
+export  INSTALL_K3S_SELINUX_WARN="true"         \
+        K3S_KUBECONFIG_MODE="644"               \
+        K3S_TOKEN=$2
+        # INSTALL_K3S_EXEC="server                \
+        #     --selinux                           \
+        #     --cluster-init                      \
+        #     --bind-address=$1                   \
+        #     --advertise-address=$1"
+
+sudo curl -sfL https://get.k3s.io |             \
+        INSTALL_K3S_EXEC="server                \
+            --selinux                           \
+            --cluster-init                      \
+            --bind-address=$1                   \
+            --advertise-address=$1"             \
+        sh -
+
+# alias k='kubectl'
--- p1/scripts/runSW.sh
+++ p1/scripts/runSW.sh
@@ -2,15 +2,19 @@
 
 # for yum in CentOS-8
 # https://forketyfork.medium.com/centos-8-no-urls-in-mirrorlist-error-3f87c3466faa
-sudo sed -i -e "s|mirrorlist=|#mirrorlist=|g" /etc/yum.repos.d/CentOS-*
-sudo sed -i -e "s|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g" /etc/yum.repos.d/CentOS-*
+# sudo sed -i -e "s|mirrorlist=|#mirrorlist=|g" /etc/yum.repos.d/CentOS-*
+# sudo sed -i -e "s|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g" /etc/yum.repos.d/CentOS-*
 
-echo "192.168.56.110 mdulcieS" >> /etc/hosts
+echo "$1 mdulcieS" >> /etc/hosts
+
 # install k3s
-# curl -sfL htt/      
+export    K3S_KUBECONFIG_MODE="644"     \
+          K3S_TOKEN=$2                  \
+          
 
-# curl -sfL https://get.k3s.io |               \
-     # K3S_KUBECONFIG_MODE="644"               \
-     # K3S_URL="https://192.168.56.110:6443"     sh -
-     # K3S_TOKEN=mynodetoken                   \
-     # sh -
+curl -sfL https://get.k3s.io |          \
+     INSTALL_K3S_EXEC="agent            \
+          --server https://$1:6443"     \
+     sh -s --
+
+# alias k='kubectl'
